7215831711aac51747d023e646c9fabd0d562aeb
gautamsen117 <gautamsen117@gmail.com>
2023-03-07 13:58:46 +0600

    loops-practice-problem

---
+++ array-loop.js
@@ -0,0 +1,14 @@
+/* for(var i = 0; i < 10; i++){
+    console.log(i);
+} */
+// target: display every elements of an array
+var numbers = [12, 34, 56, 43, 22, 66, 87, 33, 234, 11, 111];
+
+/* for(var i = 0; i < 7; i++){
+    var number = numbers[i];
+    console.log(number);
+} */
+for(var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    console.log(number);
+}
---
+++ for.js
@@ -0,0 +1,18 @@
+// for loop
+/* var roastGiven = 0;
+for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log('Roast Den, Please!!!');
+} */
+
+// simple version of for loop
+/* for(var i=0; i<7; i++){
+    console.log(i);
+} */
+
+/* for(var i = 1; i <= 10; i++){
+    console.log(i);
+} */
+
+for(var i = 0; i <= 10; i+=2){
+    console.log(i);
+}
